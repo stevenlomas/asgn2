e41bbca5da99c99da5342f7ad4a25140d12923ba
Steven Lomas <ldsiphons@hotmail.com>
2013-05-23 14:17:18 +1000

    Departing Train
    Added functions firstCarriage and nextCarriage, which both return a
    RollingStock object

--- src/asgn2Train/DepartingTrain.java
+++ src/asgn2Train/DepartingTrain.java
@@ -2,12 +2,31 @@ package asgn2Train;
 
 public class DepartingTrain {
 
+	DepartingTrain() {
+		// Constructs a (potential) train object containing no carriages (yet)
+	}
+
+	// Returns the first carriage on the train (which must be a locomotive)
+	public RollingStock::firstCarriage() {
+		// Empty
+		RollingStock test = new RollingStock;
+		return test;
+	}
+
+	/* Returns the next carriage in the train after the one returned by the
+		immediately preceding call to either this method or method firstCarriage */
+	public RollingStock::nextCarriage() {
+		// dfdf
+		RollingStock test = new RollingStock;
+		return test;
+	}
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	/* public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		// Test comment, for merging with live repo
+	} */
 
 }

1e83e3dc971e399a4b0b3a1c6c8ead6a60b25065
Grant <Grant@172.19.16.26>
2013-05-24 10:26:02 +1000

    Added exception class and package

---
+++ src/asgn2Exceptions/TrainException.java
@@ -0,0 +1,17 @@
+package asgn2Exceptions;
+
+public class TrainException {
+
+	public TrainException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

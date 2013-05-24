7fc8cafcbe0014c3f178be9be2e2e8ec2e5a559c
Grant <Grant@172.19.16.26>
2013-05-24 10:29:23 +1000

    Added testing package and classes

---
+++ src/asgn2Tests/RollingStockTests.java
@@ -0,0 +1,17 @@
+package asgn2Tests;
+
+public class RollingStockTests {
+
+	public RollingStockTests() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/asgn2Tests/TrainTests.java
@@ -0,0 +1,17 @@
+package asgn2Tests;
+
+public class TrainTests {
+
+	public TrainTests() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

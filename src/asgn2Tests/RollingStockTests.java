3a4dab3b3b8a2d4579ad9f4aad1076bb9964e047
Grant O'Meara <Grantly93@hotmail.com>
2013-05-27 16:11:44 +1000

    Added methods (no actual function though) to the test classes, taken from the lessons learned images that give an idea on what they expect us to test.

--- src/asgn2Tests/RollingStockTests.java
+++ src/asgn2Tests/RollingStockTests.java
@@ -1,22 +1,143 @@
 package asgn2Tests;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import asgn2RollingStock.RollingStock;
+import asgn2RollingStock.Locomotive;
+import asgn2RollingStock.FreightCar;
+import asgn2RollingStock.PassengerCar;
+
 /**
  * 
  * @author Grant O'Meara
  *
  */
 public class RollingStockTests {
-
-	public RollingStockTests() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
+	
+	private RollingStock testRollingStock;
+	
+	/* 
+	 * may not need this, depends on the implementation
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*@Before
+	public void setUp() {
+		testRollingStock = new RollingStock(0);
+	}*/
+	
+	@Test
+	public void invalidGrossWeightDisallowed() {
+		//test
 	}
-
+	
+	@Test
+	public void invalidLocomotivePower() {
+		//test
+	}
+	
+	@Test
+	public void invalidLocomotiveTypeDetected() {
+		//test
+	}
+	
+	@Test
+	public void invalidNumberAlighting() {
+		//test
+	}
+	
+	@Test
+	public void invalidNumberBoarding() {
+		//test
+	}
+	
+	@Test
+	public void invalidNumberOfSeats() {
+		//test
+	}
+	
+	@Test
+	public void locomotivePowerTooLow() {
+		//test
+	}
+	
+	@Test
+	public void locomotivePrintsCorrectly() {
+		//test
+	}
+	
+	@Test
+	public void nobodyGetsOff() {
+		//test
+	}
+	
+	@Test
+	public void nobodyGetsOn() {
+		//test
+	}
+	
+	@Test
+	public void noExtraRollingStockPublicConstructor() {
+		//test
+	}
+	
+	@Test
+	public void noExtraRollingStockPublicFields() {
+		//test
+	}
+	
+	@Test
+	public void noExtraRollingStockPublicMethods() {
+		//test
+	}
+	
+	@Test
+	public void noOneOnBoardInitially() {
+		//test
+	}
+	
+	@Test
+	public void noSeatsAllowed() {
+		//test
+	}
+	
+	@Test
+	public void numberOfSeatsRemembered() {
+		//test
+	}
+	
+	@Test
+	public void passengerCarPrintsCorrectly() {
+		//test
+	}
+	
+	@Test
+	public void passengersAccumulate() {
+		//test
+	}
+	
+	@Test
+	public void passengersCanAlight() {
+		//test
+	}
+	
+	@Test
+	public void powerCalculatedCorrectly() {
+		//test
+	}
+	
+	@Test
+	public void tooManyAlighting() {
+		//test
+	}
+	
+	@Test
+	public void validFreightCarTypes() {
+		//test
+	}
+	
+	@Test
+	public void validLocomotiveCodes() {
+		//test
+	}
 }
+
--- src/asgn2Tests/TrainTests.java
+++ src/asgn2Tests/TrainTests.java
@@ -1,22 +1,143 @@
 package asgn2Tests;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import asgn2Train.DepartingTrain;
+import asgn2RollingStock.RollingStock;
+import asgn2RollingStock.Locomotive;
+import asgn2RollingStock.FreightCar;
+import asgn2RollingStock.PassengerCar;
+
 /**
  * 
  * @author Grant O'Meara
  *
  */
 public class TrainTests {
-
-	public TrainTests() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
+	
+	private DepartingTrain testTrain;
+	
+	/* 
+	 * may not need this, depends on the implementation
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*@Before
+	public void setUp() {
+		testTrain = new DepartingTrain();
+	}*/
+	
+	@Test
+	public void assembleValidTrain() {
+		//test
 	}
-
+	
+	@Test
+	public void cantIterateThroughCarriages() {
+		//test
+	}
+	
+	@Test
+	public void freightBeforePassengers() {
+		//test
+	}
+	
+	@Test
+	public void negativePassengerBoarding() {
+		//test
+	}
+	
+	@Test
+	public void noCarriagesInitially() {
+		//test
+	}
+	
+	@Test
+	public void noExtraPublicDepartingTrainConstructor() {
+		//test
+	}
+	
+	@Test
+	public void noExtraPublicDepartingTrainFields() {
+		//test
+	}
+	
+	@Test
+	public void noExtrapublicDepartingTrainMethods() {
+		//test
+	}
+	
+	@Test
+	public void noLocomotiveFirst() {
+		//test
+	}
+	
+	@Test
+	public void noPassengersIntitially() {
+		//test
+	}
+	
+	@Test
+	public void noSeatsInitially() {
+		//test
+	}
+	
+	@Test
+	public void noShuntingWithPassengers() {
+		//test
+	}
+	
+	@Test
+	public void nullTrainCanMove() {
+		//test
+	}
+	
+	@Test
+	public void nullTrainPrintsCorrectly() {
+		//test
+	}
+	
+	@Test
+	public void numberOfSeatsCountedCorrectly() {
+		//test
+	}
+	
+	@Test
+	public void passengersCanBoard() {
+		//test
+	}
+	
+	@Test
+	public void removeCarriages() {
+		//test
+	}
+	
+	@Test
+	public void removeTooMany() {
+		//test
+	}
+	
+	@Test
+	public void tooManyPassengers() {
+		//test
+	}
+	
+	@Test
+	public void trainCanBarelyMove() {
+		//test
+	}
+	
+	@Test
+	public void trainExactlyFull() {
+		//test
+	}
+	
+	@Test
+	public void trainTooHeavy() {
+		//test
+	}
+	
+	@Test
+	public void twoLocomotives() {
+		//test
+	}
 }

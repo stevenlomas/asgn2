dba5d3cad57a66db9e44ed4396e578c0e1498b16
Steven Lomas <ldsiphons@hotmail.com>
2013-05-27 21:37:12 +1000

    Updated - added functionality
    Stupidly forgot to make the error class do anything.
    
    Fixed - now properly hands errors to Java Exception class.

--- src/asgn2Exceptions/TrainException.java
+++ src/asgn2Exceptions/TrainException.java
@@ -8,12 +8,19 @@ public class TrainException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * Empty exception
+	 */
+	public TrainException() {
+		super();
+	}
+	/**
 	 * Constructs a new TrainException object
 	 * 
 	 * @param message - an informative message describing the
 	 * cause of the problem
 	 */
 	public TrainException(String message) {
+		super(message);
 	}
 	
 }
